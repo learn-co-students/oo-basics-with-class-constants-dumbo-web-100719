6b0f6564533ad7101aaa0de991265f2383b2122a
Raq Robinson <robin.raq@gmail.com>
2019-10-09 13:12:39 +0000

    Done.

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -19,7 +19,7 @@ describe 'Shoe' do
       brands = ["Uggs", "Rainbow", "Nike", "Nike"]
       brands.each do |brand|
         Shoe.new(brand)
-        binding.pry
+        
       end
 
       expect(Shoe::BRANDS.size).to eq(3)
