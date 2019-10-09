a46a7d32aa0258d16e3e66c6db799b2bfbb9b966
AlinaArakelyan <alinaarakelyan90@gmail.com>
2019-10-09 11:21:15 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,15 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
+
+
+  GENRES = []
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
+
 
   def initialize(title)
     @title = title
